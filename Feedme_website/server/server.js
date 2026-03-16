b55b3ae6e612235561262d95bb635822339a7de2
Scott-Dyer <scott.dyer@my.jcu.edu.au>
2026-03-17 01:21:31 +1000

    Change db connection to pool to handle multiple requests

--- Feedme_website/server/server.js
+++ Feedme_website/server/server.js
@@ -11,19 +11,23 @@ const PORT = process.env.PORT || 80;
 
 // ================= DATABASE =================
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: 3306
+  port: 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
   if (err) {
     console.error("Database connection failed:", err);
   } else {
     console.log("Connected to MySQL");
+    connection.release();
   }
 });
 
